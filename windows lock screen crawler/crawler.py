17fd970dfa0086db0115ef26749a0aa6b7ded91e
Jeong Min Lim <limjeongmin@wustl.edu>
2018-12-25 09:39:36 +0900

    Update crawler.py
    Had to install Pillow (PIL fork)
    TODO: Try to find a way to run the script without any external script, or distribute with package included.
    TODO: Make a standalone script

--- windows lock screen crawler/crawler.py
+++ windows lock screen crawler/crawler.py
@@ -3,28 +3,13 @@ import os
 from shutil import copyfile
 import platform
 import sys
-import imageio.imread
-
-# TODO: images normally come in two resolution: mobile and desktop
-# Measure the picture sizes and ask user which image size to keep. (1, 2, or both)
-# Mobile images are narrow and tall, desktop images are wide
-# DONE TODO: Detect which system drive user's system drive is
-# DONE TODO: Show default dir and prompt user if he or she wants to change it to custom
-# DONE TODO: Detect user OS and abort if not windows
-# DONE TODO: add known icon files in tuple to filter
+from PIL import Image
 
 # TODO IDEA: Initial setup to set export directories and choose correct username, and automatically run every week or so
-# external library needed? Preferably not...
+# external library needed? Pillow (PIL)
 
 # Check letter of windows System Drive
 drive_letter = os.environ['systemdrive']
-
-# Tuple to hold commonly know icon files
-known_not_image = ('6981fe49f8861ed5fc2a9bbfda8433c94cc0effc29e5f39ee6ee5f0dcdb7cdb6',
-                   '5a8986d2cb035b1101a5c42f36b02c0ffa398f2993c002d11c202bcd55423cfc',
-                   '0c0eede1394421e9368ca381b006710a6eeaa6f59cfac0e2710847e01b6b9dec',
-                   '9d97c0789c1a62b14031c0918cbc04eaebf6046b4c3ea9f0874d1346929b6889',
-                   '62ec7a142f9bfa27d1c4f74e0b1f5c9297d1023f199577eff2c16d3100824d43',)
 
 # Check if user is running windows.
 is_Windows = platform.system() == 'Windows'
@@ -36,7 +21,7 @@ if not is_Windows:
 
 # Check for username
 your_username = getpass.getuser()
-prompt = 'Script detected "{}" is your username. Type "n" if incorrect: '.format(your_username)
+prompt = 'Script detected "{}" is your username. Type "n" if incorrect. Press any key to continue...: '.format(your_username)
 raw_is_correct_username = input(prompt)
 
 # Prompt user to input username if the name script found is not the user's username
@@ -51,22 +36,23 @@ name_list = os.listdir(file_dir)
 src_common = os.getcwd()
 
 # Ask user where to copy the images
-is_default_dir = input(
-    'Default directory to copy image files is C:/Users/{}/Documents/Windows_spotlight/. Is this okay?: (y/n) '
-        .format(your_username))
-if is_default_dir == 'y':
-    dst_common = '{}/Users/{}/Documents/Windows_spotlight/'.format(drive_letter, your_username)
-else:
-    dst_common = 'Some directory'
-    # TODO open file dialog to ask user to select directory
+# is_default_dir = input(
+#     'Default directory to copy image files is C:/Users/{}/Documents/Windows_spotlight/. Is this okay?: (y/n) '
+#         .format(your_username))
+# if is_default_dir == 'y':
+dst_common = '{}/Users/{}/Documents/Windows_spotlight/'.format(drive_letter, your_username)
+# else:
+#     dst_common = 'Some directory'
+#     # TODO open file dialog to ask user to select directory
 
-# Ask which resolution to save
-# TODO: how to accept user prompts (e,g, out of 3 options)
+# Ask which resolution to save.
 # Normally icons are square (1x1)
 # Wallpapers are generally 1920*1080 and 1080*1920
-# So resolution filtering seems to be enough? No need to keep track of icons
-# No need to change file name
-# res_target = input()
+
+save_option = 0
+while save_option not in ['1', '2', '3']:
+    save_option = input('Which resolution would you save? \n1: Landscape (1920 * 1080)'
+                        '\n2: Portrait (1080*1920)\n3: Both\n')
 
 # Iterate through the target folder and copy any possible images.
 for file in name_list:
@@ -78,13 +64,22 @@ for file in name_list:
     if file_size > 50000:
         new_image = file + '.jpg'
         src = os.path.join(src_common, file)
+        src_ext = os.path.join(src_common, new_image)
         dst = os.path.join(dst_common, new_image)
+        im = Image.open(src)
+        width, height = im.size
 
-        # TODO Check for dimensions and omit if it is square (icon)
-        # TODO: Which library to use (or is one necessary at all?)
-        # height, width, channels = imageio.imread(new_image).shape
-        # if width == height:
-        #     continue
+        # filter out icons
+        if width == height:
+            continue
+
+        # save only desired resolutions
+        if save_option == '1':
+            if width < height:
+                continue
+        elif save_option == '2':
+            if width > height:
+                continue
 
         if not os.path.exists(dst_common):
             os.makedirs(dst_common)
