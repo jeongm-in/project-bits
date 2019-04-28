0cfd4e5a7d133baeea2451aaaadb7284fc44726c
jeongm <limjeongmin@wustl.edu>
2019-04-28 17:03:59 -0500

    sssss`

--- data/script.js
+++ data/script.js
@@ -6,96 +6,96 @@ const HELL = {
     },
     "1": {
         "title": "Karmascope Terrace",
-        "subitle": "Mirror that shines on all people of the wrold is hung high.",
-        "description": "Any good and evil, merit and misstep are shown without discrepancy. You may have a mouth of steel and deny anything, but cannot deny the obvious facts shown on the mirror. If you were filial to your ancestors, your face will be reflected on your own and virtuous light will pervade the entire world of darkness. You bypass all the purgatory and will be reborn as human again. If you lived life full of evil, never respected parents and elders, you may reborn as an animal or sink down to hell and pay your price depending on your karma."
+        "subitle": "High hung mirror that shines on all people of the world.",
+        "description": "Karmascope will show any good and evil, merit and misstep without discrepancy. You may have a mouth of steel and deny anything, but you cannot deny the obvious facts shown on the mirror. If you were filial to your ancestors, your face will be reflected on your own and virtuous light will pervade the entire world of darkness. You may bypass all the purgatory and reborn as human again. If you lived life full of evil, never respected parents and elders, you may reborn as an animal or sink down to hell and pay your price depending on your karma."
     },
     "2": {
         "title": "Wasted Money Mountain",
-        "subitle": "Mountain of paper money towers ten thousand feet",
-        "description": ""
+        "subitle": "Mountain of paper money towering ten thousand feet.",
+        "description": "Stupid and foolish people who accumulated bad karma burn paper money in a futile attempt to redeem their sins after death. All those useless moneys are piled up here. This is a reminder that you need good merit, not fake paper money."
     },
     "3": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+        "title": "Pavillion for Stripping off Clothes",
+        "subitle": "People's clothes and skins are piled up high.",
+        "description": "People who killed birds and beasts, removed their pelt and chopped their legs to sell in the marketplace, you will be stripped off of clothes and skins here. "
     },
     "4": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+        "title": "Cold Ice Pond",
+        "subitle": "Pond piled high with ice.",
+        "description": "Sinners who had enough money but did not help the poor and the needy are sent here. If you don't provide clothing for your parents, commit arson or kill people, you will suffer here pierced by the freezing air."
     },
     "5": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+        "title": "Divine Rooster Mountain",
+        "subitle": "Roosters with bronze bills and iron spurs peck sinners' eyes.",
+        "description": "People who had eyes that can perfectly clearly but did not use them the right way are sent here. If you leered at the wives of others, read obscene books and heard lewd songs, or day and night engaged in gambling, roosters will eat your eyes. You will not be able to see anything for eternity."
     },
     "6": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+        "title": "Turning into Beast Depot",
+        "subitle": "Farmstead with a lot of pens full of animals.",
+        "description": "Souls are transformed into donkeys, mules, and other animals. Those who harmed others to benefit themselves are sent here, transformed into animals to pay back the debts. If you enjoyed salaries but failed to show loyalty, accepted bribes and treated common people cruelly, did not pay taxes in full, cheated, deceived, beguiled and bamboozled peoples' hearts, gave too little and demanded too much, you are likely to end up here."
     },
     "7": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+        "title": "Vats of Boiling Oil",
+        "subitle": "Vats of boliling oil where sinners are pushed into.",
+        "description": "Sinners are pushed into the vats of oil with pitchforks. Their flesh would dissolve and bones would melt. If you were delighted in buying fishes and animals, cut them up and dumped in a pan of oil to cook and roast, you should pay back the lives of these creatures. All you cared about was your own tounges and stomaches, without any considerations to the virtue of loving life. It pleased your palate to cook sentient beings while you lived. Now you shall be fried yourself."
     },
     "8": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+        "title": "Pond of Filth and Blood",
+        "subitle": "Pond full of filth and blood guarded by demons.",
+        "description": "People who did not abide by the right way but craved earthly delights-wine, sex, riches, and honor are sentenced to suffer in this place. If you created much evil karma, did not practice recitation of Amitabha Buddha's name, disobeyed parents, sought for lust and sex, you are to suffer in this pond for eternity."
     },
     "9": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+        "title": "Slippery Oil Mountain",
+        "subitle": "Mountain covered in oily sand and gravel, with no grass or trees.",
+        "description": "Sinful souls are forced to climb the mountain only to slip to the bottom and then beaten. If you had plenty of money but refused to build bridtes, repair roads, donate oil for lamps to light the roads, refused to yield to elderly and handicapped, or leveled grave mounds for personal use, you shall climb this mountain for eternity."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "10": {
+        "title": "Hometown Gazing Terrace",
+        "subitle": "Terrace to see families left behind in the world of light.",
+        "description": "You now know everything is insubstantial. Your chest may be filled with coins and silks, but you cannot take them with you here. Everyone would face the same fate you met, but there is no way you can talk to them and warn them."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "11": {
+        "title": "Place for Ripping out Hearts",
+        "subitle": "Hill full of iron pillars with sinners tied to them.",
+        "description": "Sinners are tied to an iron pillar and demon soldiers will rip their heart out. Those who were disobedient, rebellious, treacherous, thieving, filled with lascivious lust, slandered the Right Path and adhered to perverse teachings will be sent here."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "12": {
+        "title": "City of People Who Died Unjustly",
+        "subitle": "City full of people with wrongful death",
+        "description": "All wronged souls who have been hindered by their karma are sent here. Those who were killed by others, flood and fire, rage and frustration, suicide shall stay in this somber city until the cycle of enmity and revenge has been cleared. Only after then they would have their cases decided on. All wrongs are revenged and all feuds are played out eventually."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "13": {
+        "title": "Sawing in Half Terrace",
+        "subitle": "Sinners are sawed in half.",
+        "description": "Sinners who composed lewd lyrics, sang racy songs, pursued romantic affairs and perverse adulteries, mistook illusion for reality and could no longer uphold their chastity are sent here to be sawed in two halves."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "14": {
+        "title": "Tounge Extraction Field",
+        "subitle": "Peoples' chests are opened up and tounges are extracted.",
+        "description": "Sinners who used their talent at writing to deceive officials and bamboozle superiors, sinners who used their smart mouths to spread lies, slander and talk black into white will suffer here."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "15": {
+        "title": "Mountain of Steel Swords",
+        "subitle": "Tall mountain full of sword and sharp knives.",
+        "description": "Cheats and crooks are forced to climb up the mountain, bleeding from their wounds. Those who made and sold fake medicine, faked articles to cheat people out of money, seduced others to go whoring and gambling, broke others' marriages will suffer here."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "16": {
+        "title": "Village of Viscious Dogs",
+        "subitle": "Dogs that resemble tigers rush at sinners.",
+        "description": "Sinners who conducted malicious affairs in secret chambers, had sex with married women, did not worship gods are sent here to escape from ferocious dogs forever."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "17": {
+        "title": "Mortar to Pound Sinners",
+        "subitle": "Mortars to pound sinners like rice until they become new.",
+        "description": "People who dug up graves and used bones to make objects and sold them, people who tampered balances and altered measures, people who didn't believe in purifying the body are put in a mortar and pounded."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "18": {
+        "title": "Upside-Down Grinder",
+        "subitle": "Grinder where sinners are fed head to toe",
+        "description": "Sinners who did not venerate the Three Jewels, slandered the true teaching, bullyed the good and virtuous, envied the rich and reviled the poor, and schemed to deceive the weak are sent here. Their guts will shatter, skin and flesh will turn into pulp, and bones will turn into frost."
     },
-    "": {
-        "title": "",
+    "19": {
+        "title": "Granny Meng's Cottage",
         "subitle": "",
         "description": ""
     },
