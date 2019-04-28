f29389f853f0c5f0c875d4a6c3dada5799880d9c
jeongm <limjeongmin@wustl.edu>
2019-04-28 16:00:16 -0500

    ssavvee

--- data/script.js
+++ data/script.js
@@ -4,133 +4,154 @@ const HELL = {
         "subitle": "The border between the World of Light and Dark.",
         "description": "Guarded by the prison lord, anyone who is recently dead shall pass through this gate. You are all alone and by yourself, abandoned from your family and loved ones in the world of light. This is the beginning of your journey."
     },
-    "1":{
+    "1": {
         "title": "Karmascope Terrace",
         "subitle": "Mirror that shines on all people of the wrold is hung high.",
-        "description":"Any good and evil, merit and misstep are shown without discrepancy. You may have a mouth of steel and deny anything, but cannot deny the obvious facts shown on the mirror. If you were filial to your ancestors, your face will be reflected on your own and virtuous light will pervade the entire world of darkness. You bypass all the purgatory and will be reborn as human again. If you lived life full of evil, never respected parents and elders, you may reborn as an animal or sink down to hell and pay your price depending on your karma."
+        "description": "Any good and evil, merit and misstep are shown without discrepancy. You may have a mouth of steel and deny anything, but cannot deny the obvious facts shown on the mirror. If you were filial to your ancestors, your face will be reflected on your own and virtuous light will pervade the entire world of darkness. You bypass all the purgatory and will be reborn as human again. If you lived life full of evil, never respected parents and elders, you may reborn as an animal or sink down to hell and pay your price depending on your karma."
     },
-    "2":{
+    "2": {
         "title": "Wasted Money Mountain",
         "subitle": "Mountain of paper money towers ten thousand feet",
-        "description":""
+        "description": ""
     },
-    "3":{
+    "3": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "4":{
+    "4": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "5":{
+    "5": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "6":{
+    "6": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "7":{
+    "7": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "8":{
+    "8": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "9":{
+    "9": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     },
-    "":{
+    "": {
         "title": "",
         "subitle": "",
-        "description":""
+        "description": ""
     }
-    
+
 }
 
+let index = 0;
 
-let reload = ()=>{
-    let ak = document.getElementsByClassName("idd")[0].value;
+
+
+
+reload = (ak) => {
     const thisHell = HELL[ak];
     document.getElementById('title').innerHTML = thisHell['title'];
     document.getElementById('description').innerHTML = thisHell['description'];
 }
 
-reload();
+reload(0);
+// if(!first){
+//     first = true;
+// }
 
-document.getElementById("btn-right").addEventListener('click',()=>{
-    alert("aa");
-    let index = document.getElementsByClassName("idd")[0].value;
-    index = (index + 1) % 30;
-    document.getElementsByClassName("idd")[0].value = index;
-    reload();
+document.getElementById("btn-right").addEventListener('click', () => {
+    index = index + 1;
+    if (index > 30) {
+        index = 29;
+    }
+    reload(index);
+}, false);
+
+
+document.getElementById("btn-left").addEventListener('click', () => {
+    index = index - 1;
+    if (index < 0) {
+        index = 0;
+    }
+    reload(index);
+}, false);
+
+
+document.getElementById("btn-home").addEventListener('click', () => {
+    index = 0;
+    reload(index);
 }, false);
