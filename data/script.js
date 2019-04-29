892be5261a23b6cb43102b5a8a6523814f815c58
jeongm <limjeongmin@wustl.edu>
2019-04-29 00:50:53 -0500

    save

--- data/script.js
+++ data/script.js
@@ -1,123 +1,168 @@
-const HELL = {
+let HELL = {
     "0": {
         "title": "Ghost Gate Pass",
         "subitle": "The border between the World of Light and Dark.",
-        "description": "Guarded by the prison lord, anyone who is recently dead shall pass through this gate. You are all alone and by yourself, abandoned from your family and loved ones in the world of light. This is the beginning of your journey."
+        "description": "Guarded by the prison lord, anyone who is recently dead shall pass through this gate. You are all alone and by yourself, abandoned from your family and loved ones in the world of light. This is the beginning of your journey.",
+        "marked": "safe"
     },
     "1": {
         "title": "Karmascope Terrace",
         "subitle": "High hung mirror that shines on all people of the world.",
-        "description": "Karmascope will show any good and evil, merit and misstep without discrepancy. You may have a mouth of steel and deny anything, but you cannot deny the obvious facts shown on the mirror. If you were filial to your ancestors, your face will be reflected on your own and virtuous light will pervade the entire world of darkness. You may bypass all the purgatory and reborn as human again. If you lived life full of evil, never respected parents and elders, you may reborn as an animal or sink down to hell and pay your price depending on your karma."
+        "description": "Karmascope will show any good and evil, merit and misstep without discrepancy. You may have a mouth of steel and deny anything, but you cannot deny the obvious facts shown on the mirror. If you were filial to your ancestors, your face will be reflected on your own and virtuous light will pervade the entire world of darkness. You may bypass all the purgatory and reborn as human again. If you lived life full of evil, never respected parents and elders, you may reborn as an animal or sink down to hell and pay your price depending on your karma.",
+        "marked": "safe"
     },
     "2": {
         "title": "Wasted Money Mountain",
         "subitle": "Mountain of paper money towering ten thousand feet.",
         "description": "Stupid and foolish people who accumulated bad karma burn paper money in a futile attempt to redeem their sins after death. All those useless moneys are piled up here. This is a reminder that you need good merit, not fake paper money."
+        , "marked": "false"
     },
     "3": {
         "title": "Pavillion for Stripping off Clothes",
         "subitle": "People's clothes and skins are piled up high.",
         "description": "People who killed birds and beasts, removed their pelt and chopped their legs to sell in the marketplace, you will be stripped off of clothes and skins here. "
+        , "marked": "false"
     },
     "4": {
         "title": "Cold Ice Pond",
         "subitle": "Pond piled high with ice.",
         "description": "Sinners who had enough money but did not help the poor and the needy are sent here. If you don't provide clothing for your parents, commit arson or kill people, you will suffer here pierced by the freezing air."
+        , "marked": "false"
     },
     "5": {
         "title": "Divine Rooster Mountain",
         "subitle": "Roosters with bronze bills and iron spurs peck sinners' eyes.",
         "description": "People who had eyes that can perfectly clearly but did not use them the right way are sent here. If you leered at the wives of others, read obscene books and heard lewd songs, or day and night engaged in gambling, roosters will eat your eyes. You will not be able to see anything for eternity."
+        , "marked": "false"
     },
     "6": {
         "title": "Turning into Beast Depot",
         "subitle": "Farmstead with a lot of pens full of animals.",
         "description": "Souls are transformed into donkeys, mules, and other animals. Those who harmed others to benefit themselves are sent here, transformed into animals to pay back the debts. If you enjoyed salaries but failed to show loyalty, accepted bribes and treated common people cruelly, did not pay taxes in full, cheated, deceived, beguiled and bamboozled peoples' hearts, gave too little and demanded too much, you are likely to end up here."
+        , "marked": "false"
     },
     "7": {
         "title": "Vats of Boiling Oil",
         "subitle": "Vats of boliling oil where sinners are pushed into.",
         "description": "Sinners are pushed into the vats of oil with pitchforks. Their flesh would dissolve and bones would melt. If you were delighted in buying fishes and animals, cut them up and dumped in a pan of oil to cook and roast, you should pay back the lives of these creatures. All you cared about was your own tounges and stomaches, without any considerations to the virtue of loving life. It pleased your palate to cook sentient beings while you lived. Now you shall be fried yourself."
+        , "marked": "false"
     },
     "8": {
         "title": "Pond of Filth and Blood",
         "subitle": "Pond full of filth and blood guarded by demons.",
         "description": "People who did not abide by the right way but craved earthly delights-wine, sex, riches, and honor are sentenced to suffer in this place. If you created much evil karma, did not practice recitation of Amitabha Buddha's name, disobeyed parents, sought for lust and sex, you are to suffer in this pond for eternity."
+        , "marked": "false"
     },
     "9": {
         "title": "Slippery Oil Mountain",
         "subitle": "Mountain covered in oily sand and gravel, with no grass or trees.",
         "description": "Sinful souls are forced to climb the mountain only to slip to the bottom and then beaten. If you had plenty of money but refused to build bridtes, repair roads, donate oil for lamps to light the roads, refused to yield to elderly and handicapped, or leveled grave mounds for personal use, you shall climb this mountain for eternity."
+        , "marked": "false"
     },
     "10": {
         "title": "Hometown Gazing Terrace",
         "subitle": "Terrace to see families left behind in the world of light.",
         "description": "You now know everything is insubstantial. Your chest may be filled with coins and silks, but you cannot take them with you here. Everyone would face the same fate you met, but there is no way you can talk to them and warn them."
+        , "marked": "safe"
     },
     "11": {
         "title": "Place for Ripping out Hearts",
         "subitle": "Hill full of iron pillars with sinners tied to them.",
         "description": "Sinners are tied to an iron pillar and demon soldiers will rip their heart out. Those who were disobedient, rebellious, treacherous, thieving, filled with lascivious lust, slandered the Right Path and adhered to perverse teachings will be sent here."
+        , "marked": "false"
     },
     "12": {
         "title": "City of People Who Died Unjustly",
         "subitle": "City full of people with wrongful death",
         "description": "All wronged souls who have been hindered by their karma are sent here. Those who were killed by others, flood and fire, rage and frustration, suicide shall stay in this somber city until the cycle of enmity and revenge has been cleared. Only after then they would have their cases decided on. All wrongs are revenged and all feuds are played out eventually."
+        , "marked": "safe"
     },
     "13": {
         "title": "Sawing in Half Terrace",
         "subitle": "Sinners are sawed in half.",
         "description": "Sinners who composed lewd lyrics, sang racy songs, pursued romantic affairs and perverse adulteries, mistook illusion for reality and could no longer uphold their chastity are sent here to be sawed in two halves."
+        , "marked": "false"
     },
     "14": {
         "title": "Tounge Extraction Field",
         "subitle": "Peoples' chests are opened up and tounges are extracted.",
         "description": "Sinners who used their talent at writing to deceive officials and bamboozle superiors, sinners who used their smart mouths to spread lies, slander and talk black into white will suffer here."
+        , "marked": "false"
     },
     "15": {
         "title": "Mountain of Steel Swords",
         "subitle": "Tall mountain full of sword and sharp knives.",
         "description": "Cheats and crooks are forced to climb up the mountain, bleeding from their wounds. Those who made and sold fake medicine, faked articles to cheat people out of money, seduced others to go whoring and gambling, broke others' marriages will suffer here."
+        , "marked": "false"
     },
     "16": {
         "title": "Village of Viscious Dogs",
         "subitle": "Dogs that resemble tigers rush at sinners.",
         "description": "Sinners who conducted malicious affairs in secret chambers, had sex with married women, did not worship gods are sent here to escape from ferocious dogs forever."
+        , "marked": "false"
     },
     "17": {
         "title": "Mortar to Pound Sinners",
         "subitle": "Mortars to pound sinners like rice until they become new.",
         "description": "People who dug up graves and used bones to make objects and sold them, people who tampered balances and altered measures, people who didn't believe in purifying the body are put in a mortar and pounded."
+        , "marked": "false"
     },
     "18": {
         "title": "Upside-Down Grinder",
         "subitle": "Grinder where sinners are fed head to toe",
         "description": "Sinners who did not venerate the Three Jewels, slandered the true teaching, bullyed the good and virtuous, envied the rich and reviled the poor, and schemed to deceive the weak are sent here. Their guts will shatter, skin and flesh will turn into pulp, and bones will turn into frost."
+        , "marked": "false"
     },
     "19": {
         "title": "Granny Meng's Cottage",
-        "subitle": "",
-        "description": ""
+        "subitle": "Great hall with a terrace on either side with people drinking tea.",
+        "description": "This is the last place souls who goes through purgatory visits before crossing the Alas River. Granny Meng collected all of the herbs of the whole wide world and produces combination of teas. Granny Meng's brew confuses the soul. If you were evil, you will lose knowledge of former lives. If you followed the way but later neglected, you are brought back to the world of light and die soon again. Then you will be sentenced to torture. Finally if your root and practice are solid, your knowledge shall remain."
+        , "marked": "safe"
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "20": {
+        "title": "Bridge Across Alas River",
+        "subitle": "A bridge 81 rods long but only one and three tenths of an inch wide.",
+        "description": "One last step remains till your reincarnation; Crossing the Alas River. Treacherous, greedy, devilish, deceitful, violent, and cruel souls are beat with iron cudgels and forced to climb up the bridge. They will soon fall into the river, and will be grabbed and devoured by a frenzy of poisionus snakes. Crabs and fish will block your way. If you were fillial, brotherly, loyal, true to your word, decent, and righteous, divine immortals will guide your way across the river. "
+        , "marked": "safe"
+    }
+}
+
+
+const SIX_PATH = {
+    "0": {
+        "title": "Path of Gold",
+        "description": "You have practiced good deeds and established all kinds of hidden merits. You will rebon as human and become high officials and eminient nobles to serve as rulers and magistrates."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "1": {
+        "title": "Path of Silver",
+        "description": "You were always on guard, circumespect in actions and lived a pure and unblemished life. You will reborn as human, dress in silks and satins, and dwell in water pavillions and shady kiosks."
     },
-    "": {
-        "title": "",
-        "subitle": "",
-        "description": ""
+    "2": {
+        "title": "Path of Jade",
+
+        "description": "You will reborn as human, but will born hunchbacked, handicapped, or even blind. You will have to beg for food on the streets."
+    },
+    "3": {
+        "title": "Path of Bronze",
+        "description": "You ignored proper rites and norms and cheated people out of wealth. You will transform into an animal with horns and covered in fur. You will sleep in foul places and eat dirty food."
+    },
+    "4": {
+        "title": "Path of Stone",
+        "description": "You realied on your wealth and power to act arbitrarily. You committed adultery, ignored generational distinctions and engaged in lewd acts. You will be born from eggs, covered in moist feathers. Whenever you see humans, you will be frightened and fly up high."
+    },
+    "5": {
+        "title": "Path of Water",
+        "description": "Your sin is severe. You made a meal out of fish and shrimp, or made living from them. You will turn into one of fish and shrimp, and will be roasted and deep fried. You should remember any wealth obtained by improper means will be lost in the same way."
+    },
+    "6": {
+        "title": "Avici Hell",
+        "description": "Your sin is beyond reach. You violated the precept, killed people and stole others' riches, cursed superiors and was rebellious, resented heaven and hated earth. You will be forever trapped in the eighteenth layer in the world of darkness."
     }
 
 }
 
 let index = 0;
+let sinCount = 0;
 
 
 
@@ -126,7 +171,26 @@ reload = (ak) => {
     const thisHell = HELL[ak];
     document.getElementById('title').innerHTML = thisHell['title'];
     document.getElementById('description').innerHTML = thisHell['description'];
+    if (thisHell.marked == "true") {
+        document.getElementById("btn-home").style.backgroundColor = "#FF0000";
+    } else if (thisHell.marked == "false") {
+        document.getElementById("btn-home").style.backgroundColor = "#343a40";
+    } else {
+        document.getElementById("btn-home").style.backgroundColor = "#17a2b8";
+    }
 }
+
+
+
+
+printResult = (ak) => {
+    const thisResult = SIX_PATH[ak];
+    document.getElementById('title').innerHTML = thisResult["title"];
+    document.getElementById("buttons").style.display = "hidden";
+    document.getElementById("wheel").style.display = "flex";
+    document.getElementById('description').innerHTML = thisResult['description'];
+}
+
 
 reload(0);
 // if(!first){
@@ -135,10 +199,37 @@ reload(0);
 
 document.getElementById("btn-right").addEventListener('click', () => {
     index = index + 1;
-    if (index > 30) {
-        index = 29;
+    if (index > 20) {
+        const threshold = 15 / 7;
+        const verdictIndex = sinCount;
+        let result = 0;
+        if (verdictIndex <= threshold) {
+            result = 0;
+            console.log(SIX_PATH[0]);
+        } else if (threshold < verdictIndex && verdictIndex < threshold * 2) {
+            result = 1;
+            console.log(SIX_PATH[0]);
+        } else if (threshold * 2 < verdictIndex && verdictIndex < threshold * 3) {
+            result = 2;
+            console.log(SIX_PATH[0]);
+        }else if(threshold * 3< verdictIndex && verdictIndex < threshold * 4){
+            result = 3;
+            console.log(SIX_PATH[0]);
+        }else if(threshold * 4< verdictIndex && verdictIndex < threshold * 5){
+            result = 4;
+            console.log(SIX_PATH[0]);
+        }else if(threshold * 5< verdictIndex && verdictIndex < threshold * 6){
+            result = 5;
+            console.log(SIX_PATH[0]);
+        }else if(threshold * 6< verdictIndex && verdictIndex <= 15){
+            result = 6;
+            console.log(SIX_PATH[0]);
+        }
+        printResult(result);
+
+    }else{
+        reload(index);
     }
-    reload(index);
 }, false);
 
 
@@ -152,6 +243,9 @@ document.getElementById("btn-left").addEventListener('click', () => {
 
 
 document.getElementById("btn-home").addEventListener('click', () => {
-    index = 0;
-    reload(index);
+    if (HELL[index].marked == "false") {
+        sinCount += 1;
+        HELL[index].marked = "true";
+        document.getElementById("btn-home").style.backgroundColor = "#FF0000";
+    }
 }, false);
